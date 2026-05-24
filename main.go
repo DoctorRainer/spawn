64ea3dd5bb30fd2ad5d556dfa9b578db71c2ceda
Rainer Reinhardt <drryner@icloud.com>
2026-05-24 06:20:40 +0200

    use sprintf instead of templates

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
-	"text/template"
 
 	"gopkg.in/yaml.v3"
 )
@@ -14,25 +12,6 @@ type Config struct {
 	Name    string `yaml:"name"`
 	Command string `yaml:"command"`
 }
-
-const scriptTemplate = `#!/bin/sh
-# PROVIDE: {{.Name}}
-# REQUIRE: NETWORKING
-# KEYWORD: shutdown
-
-. /etc/rc.subr
-
-name={{.Name}}
-rcvar={{.Name}}_enable
-
-command="/usr/sbin/daemon"
-command_args="-r -p /var/run/{{.Name}}.pid -o /var/log/{{.Name}}.log {{.Command}}"
-
-load_rc_config $name
-: ${ {{.Name}}_enable:="NO"}
-
-run_rc_command "$1"
-`
 
 func main() {
 	data, err := os.ReadFile("demon.yaml")
@@ -52,21 +31,34 @@ func main() {
 		os.Exit(1)
 	}
 
-	tmpl, _ := template.New("rc").Parse(scriptTemplate)
-	var buf bytes.Buffer
-	tmpl.Execute(&buf, cfg)
+	script := fmt.Sprintf(`#!/bin/sh
+# PROVIDE: %s
+# REQUIRE: NETWORKING
+# KEYWORD: shutdown
+
+. /etc/rc.subr
+
+name=%s
+rcvar=%s_enable
+
+command="/usr/sbin/daemon"
+command_args="-r -p /var/run/%s.pid -o /var/log/%s.log %s"
+
+load_rc_config $name
+: ${%s_enable:="NO"}
+
+run_rc_command "$1"
+`, cfg.Name, cfg.Name, cfg.Name, cfg.Name, cfg.Name, cfg.Command, cfg.Name)
 
 	target := "/usr/local/etc/rc.d/" + cfg.Name
-	if err := os.WriteFile(target, buf.Bytes(), 0755); err != nil {
+	if err := os.WriteFile(target, []byte(script), 0755); err != nil {
 		fmt.Printf("Write error %s: %v (run as root/sudo)\n", target, err)
-		fmt.Println("\n--- script ---\n" + buf.String())
+		fmt.Println("\n--- script ---\n" + script)
 		os.Exit(1)
 	}
 
 	fmt.Printf("Created %s (+x)\n", target)
-
 	exec.Command("sysrc", cfg.Name+"_enable=YES").Run()
 	exec.Command("service", cfg.Name, "start").Run()
-
 	fmt.Printf("Enabled and started %s\n", cfg.Name)
 }
